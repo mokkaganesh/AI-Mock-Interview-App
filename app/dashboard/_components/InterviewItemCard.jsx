c0e630d7b45a90b744ae82e14ae64ac7bf55e108
MOKKA GANESH <111331830+mokkaganesh@users.noreply.github.com>
2024-08-26 15:42:50 +0530

    Update InterviewItemCard.jsx corrected button spelling

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '@/components/ui/Button'
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 function InterviewItemCard({interview}) {
